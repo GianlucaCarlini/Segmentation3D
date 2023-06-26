2fb4f9f7960400b4f12e5bb65de1e5d8d07c6971
GianlucaCarlini <gianluca.carlini@outlook.it>
2023-06-26 18:46:16 +0200

    Added metrics and lr schedulers to lightning models. Works well with n_classes > 2, with n_classes = 1 I get some indexing errors. Perhaps some squeeze() that shouldn't be there. Have to check it.

--- models/lightning_models.py
+++ models/lightning_models.py
@@ -2,12 +2,19 @@ import torch
 import pytorch_lightning as pl
 from utils.prediction_loops import predict_tensor_patches
 import numpy as np
-from typing import Union, Any, Optional
+from typing import Union, Any, Optional, Callable
 
 
 class Unet3D(pl.LightningModule):
     def __init__(
-        self, model: torch.nn.Module, loss: torch.nn.Module, *args: Any, **kwargs: Any
+        self,
+        model: torch.nn.Module,
+        loss: torch.nn.Module,
+        metrics: Callable=None,
+        optimizer: torch.optim.Optimizer = None,
+        lr_scheduler: torch.optim.lr_scheduler._LRScheduler = None,
+        *args: Any,
+        **kwargs: Any
     ) -> None:
         """Instantiate a 3D U-Net model.
 
@@ -24,7 +31,7 @@ class Unet3D(pl.LightningModule):
         super().__init__()
 
         self.in_channels = kwargs.get("in_channels", 1)
-        self.depths = kwargs.get("depths", [3, 3, 6, 3])
+        self.depths = kwargs.get("depths", [3, 3, 9, 3])
         self.embed_dim = kwargs.get("embed_dim", 32)
         self.channel_multipliers = kwargs.get("channel_multipliers", [1, 2, 4, 8])
         self.classes = kwargs.get("classes", 1)
@@ -41,6 +48,23 @@ class Unet3D(pl.LightningModule):
 
         self.loss = loss
 
+        if metrics is not None:
+            self.metrics = metrics
+        else:
+            self.metrics = None
+
+        if optimizer is not None:
+            self.optimizer = optimizer
+        else:
+            self.optimizer = torch.optim.Adam
+
+        if lr_scheduler is not None:
+            self.lr_scheduler = lr_scheduler
+        else:
+            self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR
+
+        self.training_steps = kwargs.get("training_steps", 1000)
+        self.initial_lr = kwargs.get("initial_lr", 1e-3)
         self.patch_size = kwargs.get("patch_size", (128, 128, 128))
         self.strides = kwargs.get("stride", (64, 64, 64))
         self.padding = kwargs.get("padding", "same")
@@ -52,9 +76,20 @@ class Unet3D(pl.LightningModule):
         x, y = batch
         y_pred = self(x)
 
+        B = y.size(dim=0)
+
         loss = self.loss(y, y_pred)
 
-        self.log("train_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        self.log(
+            "train_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True
+        )
+
+        if self.metrics is not None:
+
+            for name, metric in self.metrics.items():
+                metric = metric.to(y)
+                self.log(f'train_{name}', metric(y_pred, y), prog_bar=True, on_step=True, logger=True, on_epoch=True)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -74,6 +109,12 @@ class Unet3D(pl.LightningModule):
 
         self.log("val_loss", loss, prog_bar=True, logger=True)
 
+        if self.metrics is not None:
+
+            for name, metric in self.metrics.items():
+                metric = metric.to(y)
+                self.log(f'val_{name}', metric(volume_pred, y), prog_bar=True, logger=True)
+
     def predict_step(self, volume: torch.Tensor, **kwargs) -> torch.Tensor:
         """Prediction step of the model.
 
@@ -87,7 +128,13 @@ class Unet3D(pl.LightningModule):
         volume = volume.squeeze().squeeze()
 
         return predict_tensor_patches(tensor=volume, model=self.model, **kwargs)
-    
+
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
-        return optimizer
+        
+        optimizer = self.optimizer(self.parameters(), lr=self.initial_lr)
+        scheduler = self.lr_scheduler(optimizer, T_max=self.training_steps, eta_min=1e-6)
+        
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {"scheduler": scheduler, "interval": "step"},
+        }
